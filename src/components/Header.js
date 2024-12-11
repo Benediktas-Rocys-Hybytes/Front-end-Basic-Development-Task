7e36917f3e44626c1a142047c915a531653ba77a
Benediktas-Rocys-Hybytes <benediktas.rocys@hybytes.co.uk>
2024-12-11 16:14:40 +0000

    Updated header to be responsive, there is a menu toggle now

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/bootstrap.css";
 import "../styles/font-awesome.min.css";
 import "../styles/responsive.css";
@@ -6,16 +6,22 @@ import "../styles/style.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <header className="header_section">
       <div className="header_top">
         <div className="container-fluid">
           <div className="contact_nav">
-            <a href="#">
+            <a href="tel:+01123455678990">
               <i className="fa fa-phone" aria-hidden="true"></i>
               <span> Call : +01 123455678990</span>
             </a>
-            <a href="#">
+            <a href="mailto:demo@gmail.com">
               <i className="fa fa-envelope" aria-hidden="true"></i>
               <span> Email : demo@gmail.com</span>
             </a>
@@ -28,7 +34,23 @@ const Header = () => {
             <Link className="navbar-brand" to="/">
               <span>Inance</span>
             </Link>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              onClick={toggleMenu}
+              aria-expanded={isMenuOpen}
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div
+              className={`collapse navbar-collapse ${
+                isMenuOpen ? "show" : ""
+              }`}
+              id="navbarSupportedContent"
+            >
               <ul className="navbar-nav">
                 <li className="nav-item">
                   <Link className="nav-link" to="/">
